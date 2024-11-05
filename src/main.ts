7f6dbbdf8d553611897c1b921ecabd9935584bde
Burma Rysmatova <rysmatovaburma@gmail.com>
2024-11-05 17:15:25 +0600

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.setGlobalPrefix(environment.app.prefix);
 
   app.use(cookieParser.default());
